1d7935595e3c9e51054b27eacc3f56b6b18ab6ae
soichiro310 <so16.310@gmail.com>
2020-09-07 14:23:48 +0900

    [add] 画像を複数枚表示させるやつを追加

--- myCV.py
+++ myCV.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import rcParams
 
 def myImshow(img=None):
     # パスを与えた場合は，imread
@@ -23,4 +24,12 @@ def myImshow(img=None):
     # グレースケール画像だったらcmap='gray'と設定して表示
     elif img.ndim == 2:
         plt.imshow(img, cmap='gray')
+        
+def showMultipleImages(images,col,row, label_set=True, scale=(4,3)):
+    rcParams['figure.figsize'] = scale[0] * col, scale[1] * row
+    for i in range(row):
+        for j in range(col):
+            plt.subplot(row,col,col * i + j + 1)
+            plt.tick_params(labelbottom=label_set,labelleft=label_set)
+            myImshow(images[col * i + j])
     
