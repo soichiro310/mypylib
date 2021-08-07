98b29fbc4666c9099505156eaefc599640cd525a
soichiro310 <s183332@edu.tut.ac.jp>
2021-08-07 14:42:08 +0900

    bugfix in loading PIL Image

--- myCV.py
+++ myCV.py
@@ -1,25 +1,11 @@
 import os
 import cv2
+import PIL
 import matplotlib.pyplot as plt
 import numpy as np
 from pylab import rcParams
 
-def myImshow(img=None, title=None, scale=(6,4)):
-    rcParams['figure.figsize'] = scale[0], scale[1]
-    # パスを与えた場合は，imread
-    if type(img) is str:
-        img = cv2.imread(img)
-    # numpy配列だったらpass
-    elif type(img) is np.ndarray:
-        pass
-    # それ以外の型だったらエラー出してreturn
-    else:
-        print('type error ->',type(img))
-        return
-    
-    if title is not None:
-        plt.title(title)
-    
+def cvImshow(img):
     # RGB画像だったらBGRに変換して表示
     if img.ndim == 3:
         img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
@@ -28,9 +14,40 @@ def myImshow(img=None, title=None, scale=(6,4)):
     # グレースケール画像だったらcmap='gray'と設定して表示
     elif img.ndim == 2:
         plt.imshow(img, cmap='gray')
-        
+
     else :
         print('ndim error ->',img.ndim)
+
+    return
+
+def pilImshow(img):
+    plt.imshow(img)
+    return
+
+def myImshow(img=None, title=None, scale=(6,4)):
+    rcParams['figure.figsize'] = scale[0], scale[1]
+    
+    if title is not None:
+        plt.title(title)
+
+    # パスを与えた場合は，imread
+    if type(img) is str:
+        img = cv2.imread(img)
+        cvImshow(img)
+        return
+    # numpy配列だったらpass
+    elif type(img) is np.ndarray:
+        cvImshow(img)
+        return
+    elif type(img) is PIL.PngImagePlugin.PngImageFile:
+        pilImshow(img)
+        return
+    # それ以外の型だったらエラー出してreturn
+    else:
+        print('type error ->',type(img))
+        return
+    
+    
         
 def showMultipleImages(images,col=7,row=None, label_set=True, scale=(4,3)):
     if row is None:
