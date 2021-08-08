6f3c8d044a3456da37b07f646a0b3235726d271c
soichiro310 <s183332@edu.tut.ac.jp>
2021-08-08 15:51:56 +0900

    add json and pickle load

--- mypylib.py
+++ mypylib.py
@@ -2,6 +2,8 @@ import os
 import sys
 import warnings
 import glob
+import json
+import pickle
 warnings.simplefilter('ignore')
 
 def makeDir(path):
@@ -12,3 +14,14 @@ def makeDir(path):
 def getFileList(dir_path):
     return sorted(glob.glob(dir_path + '/*'))
 
+def json_load(path):
+    return json.load(open(path,"r"))
+    
+def json_dump(obj,path):
+    return json.dump(open(path,"w"))
+
+def pickle_load(path):
+    return pickle.load(open(path,"rb"))
+
+def pickle_dump(obj,path):
+    return pickle.dump(obj,open(path,"wb"))
